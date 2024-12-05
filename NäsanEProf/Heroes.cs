f1cf70c3e960e019b0ef0a8b66db55510fa61ee3
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-12-05 11:16:59 +0100

    alt+shirt+f och kommentarer :D

--- NäsanEProf/Heroes.cs
+++ NäsanEProf/Heroes.cs
@@ -5,44 +5,54 @@ namespace Hjältar
     {
         private string _namn;
         private int _hälsa;
-
-        public Hjältar(string namn, int hälsa){
+        //Konstruktor för att ge variabler värden
+        public Hjältar(string namn, int hälsa)
+        {
             _namn = namn;
             _hälsa = hälsa;
         }
-
-        public void Information(){
+        //metod för att visa informationen som hjälten har
+        public void Information()
+        {
             Console.WriteLine($"Namn: {_namn}");
             Console.WriteLine($"Hälsa: {_hälsa}");
 
         }
-        public void BytHälsa(int mängd){
-            _hälsa -= mängd;
+        //när vi skadar eller ger hälsa kallas denna
+        public void BytHälsa(int mängd)
+        {
+            _hälsa += mängd;
         }
     }
 
-    class GodHjälte : Hjältar{
+    class GodHjälte : Hjältar
+    {
 
         private int _helandeStyrka;
-
-        public GodHjälte(string namn, int hp, int helandeStyrka) : base(namn, hp){
+        //konstruktor som skickar tillbaka data till parent klassen 
+        public GodHjälte(string namn, int hp, int helandeStyrka) : base(namn, hp)
+        {
             _helandeStyrka = helandeStyrka;
         }
-
-        public void GeHälsa(Hjältar Target){
+        //ger hälsa till given hjälte
+        public void GeHälsa(Hjältar Target)
+        {
             Target.BytHälsa(_helandeStyrka);
         }
     }
 
-    class OndHjälte : Hjältar{
+    class OndHjälte : Hjältar
+    {
 
-        private int _AttackStyrka; 
-
-        public OndHjälte(string namn, int hp, int attackstyrka) : base(namn,hp){
+        private int _AttackStyrka;
+        //skickar tillbaka data till parent klassen
+        public OndHjälte(string namn, int hp, int attackstyrka) : base(namn, hp)
+        {
             _AttackStyrka = attackstyrka;
         }
-
-        public int skada(int mängd){
+        //ger tillbaka en int som anger hur mycket skada som gjorts
+        public int skada(int mängd)
+        {
             return mängd;
         }
 
--- NäsanEProf/Program.cs
+++ NäsanEProf/Program.cs
@@ -2,31 +2,35 @@
 
 Console.WriteLine("Vilken typ av hjälte vill du skapa?");
 
-    int Type;
-    string[] options = {"1", "2"};
-    string input;
-    GodHjälte God;
-    OndHjälte Ond;
+int Type;
+// en lista av möjliga alternativ spelaren kan skriva in
+string[] options = { "1", "2" };
+string input;
+GodHjälte God;
+OndHjälte Ond;
+//kollar att inputen spelaren givit är 1 eller 2 
+do
+{
+    Console.WriteLine("1 för godhjälte");
+    Console.WriteLine("2 för ondhjälte");
+    input = Console.ReadLine() ?? "";
+} while (!options.Contains(input));
 
-    do{
-        Console.WriteLine("1 för godhjälte");
-        Console.WriteLine("2 för ondhjälte");
-        input = Console.ReadLine()?? "";
-    }while(!options.Contains(input));
+Type = Convert.ToInt32(input);
 
-    Type = Convert.ToInt32(input);
+Console.WriteLine("Vilket namn ska din hjälte ha?");
+//kollar inputen och skapar en karaktär beroende på vilken man valt
+if (Type == 1)
+{
+    God = new GodHjälte(Console.ReadLine(), Random.Shared.Next(1, 4), Random.Shared.Next(1, 4));
+    God.Information();
+}
+else if (Type == 2)
+{
+    Ond = new OndHjälte(Console.ReadLine(), Random.Shared.Next(1, 4), Random.Shared.Next(1, 4));
+    Ond.Information();
+}
 
-    Console.WriteLine("Vilket namn ska din hjälte ha?");
-    
-    if(Type == 1){
-        God = new GodHjälte(Console.ReadLine(), Random.Shared.Next(1,4), Random.Shared.Next(1,4));
-        God.Information();
-    }
-    else if(Type == 2){
-        Ond = new OndHjälte(Console.ReadLine(), Random.Shared.Next(1,4), Random.Shared.Next(1,4));
-        Ond.Information();
-    }
+Console.ReadLine();
 
-    Console.ReadLine();
-    
 
